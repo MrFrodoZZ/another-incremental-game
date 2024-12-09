5e14adc9598089dd98175db10448440352f28a00
FrodoZZ <frodozz.a1@gmail.com>
2024-12-09 16:30:21 +0900

    pu-pu-puu

--- app.js
+++
@@ -1,8 +0,0 @@
-let coppers = 0;
-let copperGrowth = 1;
-myTimer = setInterval(endOfTurnCalc, 100);
-
-function endOfTurnCalc() {
-    coppers = Math.round(coppers + copperGrowth / 10).toPrecision(2);
-    document.getElementById("spnCopperValue").innerHTML = coppers;
-}
